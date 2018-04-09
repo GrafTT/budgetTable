9c28c604f858a6037e21c4db378e785d8913bd2a
Viacheslav Lamash <slava91billion@gmail.com>
2018-04-09 20:26:25 +0400

    Delete test code

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,2 @@
 import _ from 'lodash';
 import './../sass/styles.scss';
-
-
-const getHeader = () => {
-    const helloWebpack = _.join(_.values({
-        a: 'Hello',
-        b: 'webpack'
-    }), ' ');
-
-    const element = document.createElement('h1');
-
-    element.innerHTML = helloWebpack;
-
-    return element;
-};
-document.body.appendChild(getHeader());
